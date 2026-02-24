cb2200d1f2edec15a277da549f0a9628e88d304f
astrop15 <Myriam.gzara@gmail.com>
2026-02-24 15:17:02 +0100

    Update routes.php

--- apps/server/src/routes.php
+++ apps/server/src/routes.php
@@ -1,12 +1,10 @@
 <?php
 
-// GET simple
 $router->get('/', function() {
     header('Content-Type: application/json');
     echo json_encode(["message" => "Hello depuis le serveur robotique !"]);
 });
 
-// POST pour commander le robot
 $router->post('/', function() {
     header('Content-Type: application/json');
     echo json_encode(["status" => "Commande robot reçue"]);
